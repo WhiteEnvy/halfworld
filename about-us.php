d57539702e3c91a5772f80da22543af37a1a4ef0
WhiteEnvy <yamburenko.anatoliy@gmail.com>
2016-11-27 22:41:47 +0200

    about us

--- about-us.php
+++ about-us.php
@@ -1,6 +1,6 @@
-<div class="wrapper col-md-8 col-md-offset-2">
-    <h2>About us:</h2>
+<div class="wrapper col-md-8 col-md-offset-2">    
     <img src="../img/vector_hotel_logo.png" alt="">
+    <h2>HALFWORLD IS:</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo voluptatum magni atque ratione cumque, similique, voluptas iure cupiditate nihil sunt, eius mollitia quos, velit ipsa accusantium minima soluta? Quod, nesciunt?</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quia quibusdam enim eum sapiente, iste, temporibus nam quasi, sed vitae sit repudiandae. Consectetur, asperiores iusto dolore voluptatibus expedita quisquam maxime.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quia quibusdam enim eum sapiente, iste, temporibus nam quasi, sed vitae sit repudiandae. Consectetur, asperiores iusto dolore voluptatibus expedita quisquam maxime.</p>
