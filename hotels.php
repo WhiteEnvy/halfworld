95f9086b65d0e7ec0cf76a16ddf9693de13167c8
WhiteEnvy <yamburenko.anatoliy@gmail.com>
2016-11-27 16:34:19 +0200

    img

--- hotels.php
+++ hotels.php
@@ -32,7 +32,10 @@ $myhotels = $stmt->fetchAll();
    var_dump($mainUrl);
 for($i=0; $i<count($myhotels);$i++){
     var_dump($myhotels[$i]['image_url']);
-        echo "<div class='item col-md-8 col-md-offset-2'></div>"; 
+        echo "<div class='item col-md-8 col-md-offset-2'>
+            <img src='../img/".$myhotels[$i]['image_url']."' alt='' class='col-md-4'>
+            
+        </div>"; 
 }
 
 ?>
