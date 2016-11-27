b71e7984c458804788b44f67518151caa546f951
WhiteEnvy <yamburenko.anatoliy@gmail.com>
2016-11-27 16:37:29 +0200

    sdf

--- hotels.php
+++ hotels.php
@@ -29,15 +29,17 @@ $myhotels = $stmt->fetchAll();
 //        </div>";
 //}
 
-   var_dump($mainUrl);
+ 
 for($i=0; $i<count($myhotels);$i++){
-    var_dump($myhotels[$i]['image_url']);
         echo "<div class='item col-md-8 col-md-offset-2'>
             <img src='../img/".$myhotels[$i]['image_url']."' alt='' class='col-md-4'>
             <div class='description col-md-8'>
-                <h2>".$myhotels[$i]['name']."</h2>".trim(mb_strimwidth($myhotels[$i]['description'], 0, 450))."...
+                <h2>".$myhotels[$i]['name']."</h2>...
             </div>
-            
+            <a class='more' href='$mainUrl/hotels?name=".$myhotels[$i]['name']."'>
+                <p>Read more</p>
+                <i class='glyphicon glyphicon glyphicon-share-alt'></i>
+            </a>
         </div>"; 
 }
 
