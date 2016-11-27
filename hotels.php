4fcf8180eabd5409f8a8f10b55bfc0132c6233a1
WhiteEnvy <yamburenko.anatoliy@gmail.com>
2016-11-27 16:20:44 +0200

    qwds

--- hotels.php
+++ hotels.php
@@ -29,11 +29,10 @@ $myhotels = $stmt->fetchAll();
 //        </div>";
 //}
 
-
+   var_dump($mainUrl);
 for($i=0; $i<count($myhotels);$i++){
-    var_dump($myhotels[$i]);
-        echo"
-        <div class='item col-md-8 col-md-offset-2'>
+    var_dump($myhotels[$i]['image_url']);
+        echo "<div class='item col-md-8 col-md-offset-2'>
             <img src='../img/".$myhotels[$i]['image_url']."' alt='' class='col-md-4'>
             <div class='description col-md-8'>
                 <h2>".$myhotels[$i]['name']."</h2>".trim(mb_strimwidth($myhotels[$i]['description'], 0, 450))."...
@@ -43,12 +42,6 @@ for($i=0; $i<count($myhotels);$i++){
                 <i class='glyphicon glyphicon glyphicon-share-alt'></i>
             </a>
         </div>"; 
-     echo "<br>";
-     echo "<br>";
-     echo "<br>";
-     echo "<br>";
-     echo "<br>";
-     echo "test";
 }
 
 ?>
