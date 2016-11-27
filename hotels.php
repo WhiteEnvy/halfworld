631eb82173458581658be3e6432a97c2d1d6b6bf
WhiteEnvy <yamburenko.anatoliy@gmail.com>
2016-11-27 16:49:15 +0200

    mini fix

--- hotels.php
+++ hotels.php
@@ -21,7 +21,7 @@ foreach($myhotels as $q){
         <div class='item col-md-8 col-md-offset-2'>
             <img src='../img/".$q['image_url']."' alt='' class='col-md-4'>
             <div class='description col-md-8'>
-                <h2>".$q['name']."</h2>".trim(substr($q['description'], 0, 450))."...
+                <h2>".$q['name']."</h2>".trim(substr($q['description'], 0, 448))."...
             </div>
             <a class='more' href='$mainUrl/hotels?name=".$q['name']."'>
                 <p>Read more</p>
